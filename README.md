4dbc938464db8666290b61fd4afbba4279e91e73
Enkows Zhang <enkows.zhang@gmail.com>
2015-12-18 22:54:16 +0800

    update read me

---
+++ README.md
@@ -0,0 +1,6 @@
+### Run on iOS
+* copy `ios/v2ex/info.plist.example` to `ios/v2ex/info.plist`
+* Change the IP in the URL from localhost to your laptop's IP. On Mac, you can find the IP address in System Preferences / Network.
+* In Xcode select your phone as build target and press "Build and run"
+
+### Screen Shots
--- ios/v2ex/AppDelegate.m
+++ ios/v2ex/AppDelegate.m
@@ -19,8 +19,6 @@
   NSURL *jsCodeLocation;
   #if !(TARGET_IPHONE_SIMULATOR)
     NSLog(@"Running on device!");
-    // jsCodeLocation = [[NSBundle mainBundle] URLForResource:@"main" withExtension:@"jsbundle"];
-    // jsCodeLocation = [NSURL URLWithString:@"http://192.168.1.132:8081/index.ios.bundle?platform=ios&dev=true"];
     jsCodeLocation = [CodePush bundleURL];
   #else
     jsCodeLocation = [NSURL URLWithString:@"http://localhost:8081/index.ios.bundle?platform=ios&dev=true"];
